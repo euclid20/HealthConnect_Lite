7b37be16ff94d49035fa61d362ed2fd1d996f54b
faidfadjri <faidfadjri.busdev@gmail.com>
2024-05-08 20:24:18 +0700

    features: save user

--- src/models/user_model.py
+++ src/models/user_model.py
@@ -6,6 +6,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     avatar = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    first_name = db.Column(db.String(120), unique=True, nullable=False)
+    last_name = db.Column(db.String(120), unique=True, nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.avatar
--- src/routes/user_routes.py
+++ src/routes/user_routes.py
@@ -1,7 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
 
-from controllers.user_controller import get_users, store_user
-from entities.user_entity import User
+from controllers.user_controller import get_users
+from models.user_model import User
+from config.setup import db
 
 user_blueprint = Blueprint('user', __name__, url_prefix='/user');
 
@@ -11,5 +12,33 @@ def get_users_route():
 
 @user_blueprint.route('/', methods=['POST'])
 def store_user_route():
-    return store_user()
 
+    form  = request.form
+
+    # Validation
+
+    # Define the required fields
+    required_fields = ['email', 'first_name', 'last_name', 'avatar']
+    
+    # Check for missing fields
+    missing_fields = [field for field in required_fields if field not in form]
+
+    if missing_fields:
+        return jsonify({'message': 'Unprocessable content', 'error': f'Missing fields: {", ".join(missing_fields)}'}), 422    
+
+    email = form['email']
+    first_name = form['first_name']
+    last_name = form['last_name']
+    avatar = form['avatar']
+    
+    new_user = User(email=email, first_name=first_name, last_name=last_name, avatar=avatar)
+    db.session.add(new_user)
+
+    try:
+        # Commit the transaction to save the new user to the database
+        db.session.commit()
+        return jsonify({'message': 'User created successfully'}), 201
+    except Exception as e:
+        # If there's an error, rollback the transaction
+        db.session.rollback()
+        return jsonify({'error': f'Failed to create user: {str(e)}'}), 500
