5b22bc1c3724970cd23d6311a26945f62edfb75f
faidfadjri <faidfadjri.busdev@gmail.com>
2024-05-09 00:10:25 +0700

    features : add list of user and detail user

--- src/controllers/user_controller.py
+++ src/controllers/user_controller.py
@@ -6,15 +6,35 @@ from config.setup import db
 
 
 def get_users():
-    first_user = User.query.first()
-    if first_user:
-        return jsonify({
-            "id": first_user.id,
-            "avatar": first_user.avatar,
-            "email": first_user.email
-        })
+    # Get all users from the database
+    users = User.query.all()
+    if users:
+        # Serialize each user and store them in a list
+        serialized_users = []
+        for user in users:
+            serialized_users.append({
+                "id": user.id,
+                "avatar": user.avatar,
+                "email": user.email
+            })
+        # Return the list of serialized users
+        return jsonify(serialized_users)
     else:
         return jsonify({"message": "No users found"})
+    
+def get_specific_user(user_id: int):
+    user = User.query.filter_by(id=user_id).first()
+    if user:
+        # Serialize the user and return it
+        serialized_user = {
+            "id": user.id,
+            "avatar": user.avatar,
+            "email": user.email
+        }
+        return jsonify(serialized_user)
+    else:
+        return jsonify({"message": "User not found"})
+
 
 def store_user(user_data: UserEntity):
     new_user = User(email=user_data.email, first_name=user_data.first_name, last_name=user_data.last_name, avatar=user_data.avatar)
--- src/routes/user_routes.py
+++ src/routes/user_routes.py
@@ -1,36 +1,50 @@
+import os
 from flask import Blueprint, request, jsonify
-
-from controllers.user_controller import get_users, store_user
+from controllers.user_controller import get_users, store_user, get_specific_user
 from entities.user_entity import UserEntity
 
-
-user_blueprint = Blueprint('user', __name__, url_prefix='/user');
+user_blueprint = Blueprint('user', __name__, url_prefix='/user')
 
 @user_blueprint.route('/', methods=['GET'])
 def get_users_route():
     return get_users()
 
+@user_blueprint.route('/<int:user_id>', methods=['GET'])
+def get_specific_user_route(user_id):
+    return get_specific_user(user_id)
+
 @user_blueprint.route('/', methods=['POST'])
 def store_user_route():
+    # Check if the request contains a file
+    if 'avatar' not in request.files:
+        return jsonify({'message': 'Unprocessable content', 'error': 'Avatar is missing / not a file'}), 422
 
-    form  = request.form
+
+    # Handle uploading avatar file
+    avatar = request.files['avatar']
+    avatar_filename = avatar.filename
+
+    if avatar_filename == '':
+        return jsonify({'message': 'Unprocessable content', 'error': 'Avatar file is empty'}), 422
+
+    upload_dir = 'public/images'
+    if not os.path.exists(upload_dir):
+        os.makedirs(upload_dir)
+    avatar_path = os.path.join(upload_dir, avatar.filename)
+    avatar.save(avatar_path)
+
+    # Get other form fields
+    email = request.form.get('email')
+    first_name = request.form.get('first_name')
+    last_name = request.form.get('last_name')
 
     # Validation
-
-    # Define the required fields
-    required_fields = ['email', 'first_name', 'last_name', 'avatar']
-    
-    # Check for missing fields
-    missing_fields = [field for field in required_fields if field not in form]
-
-    if missing_fields:
-        return jsonify({'message': 'Unprocessable content', 'error': f'Missing fields: {", ".join(missing_fields)}'}), 422    
-
-    email = form['email']
-    first_name = form['first_name']
-    last_name = form['last_name']
-    avatar = form['avatar']
+    if not (email and first_name and last_name):
+        return jsonify({'message': 'Unprocessable content', 'error': 'Missing required fields'}), 422
 
     # Create UserEntity instance
-    user_data = UserEntity(avatar=avatar, email=email, first_name=first_name, last_name=last_name)
-    return store_user(user_data)   
+    user_data = UserEntity(avatar=avatar_filename, email=email, first_name=first_name, last_name=last_name)
+
+    # Call the controller function to store the user
+    return store_user(user_data)
+
